d3f96cb570086df7bc491422b0331060eb2ffb32
Toby Lynas <tobylynas@tobys-air.lan>
2020-12-17 09:40:07 +0000

    add list

--- pages/newPage.js
+++ pages/newPage.js
@@ -9,13 +9,20 @@ export default function newPage(){
         </Head>
 
         <main>
-            <h1 className={styles.title}>
-            <style>
-              @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
-            </style>
-              BEE CREATIVE
-              
-            </h1>
+            <div className={styles.topBar}>
+              <h1 className={styles.title}>
+                <style>
+                  @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+                </style>
+                  BEE CREATIVE
+              </h1>
+              <ul className={styles.topList}>
+                <li>Nav1</li>
+                <li>Nav2</li>
+                <li>Nav3</li>
+                <li>Nav4</li>
+              </ul>
+            </div>
         </main>
       </div>
     )
