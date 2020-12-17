4f18d1a65b13a9bcbdc62fcde79de06c5eba22a6
Toby Lynas <tobylynas@tobys-air.lan>
2020-12-17 19:13:09 +0000

    Add socials SVG and postioned

--- pages/_app.js
+++ pages/_app.js
@@ -21,7 +21,9 @@ function MyApp({ Component, pageProps }) {
       <div className={styles.footContainer}>
         <div className={styles.footer}>
           <div className={styles.footFBox}>
-            example text
+            <img src="/facebook.svg" alt="FaceBook Logo" className={styles.logo} />
+            <img src="/instagram.svg" alt="Instagram Logo" className={styles.logo} />
+            <img src="/etsy.svg" alt="Etsy Logo" className={styles.logo} />
           </div>
         </div>
       </div>
