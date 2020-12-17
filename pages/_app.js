64419663a755d16f6bd909b4bde9cc849f45028b
Toby Lynas <tobylynas@tobys-air.lan>
2020-12-17 18:18:11 +0000

    Add containing bar and set colors

--- pages/_app.js
+++ pages/_app.js
@@ -3,26 +3,28 @@ import styles from '../styles/index.module.css'
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-    <div className={styles.topBar}>
-      <h1 className={styles.title}>
-        <style>
-        @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
-        </style>
-          BEE CREATIVE
-      </h1>
-      <ul className={styles.toplist}>
-        <li className={styles.list}>Nav1</li>
-        <li className={styles.list}>Nav2</li>
-        <li className={styles.list}>Nav3</li>
-        <li className={styles.list}>Nav4</li>
-      </ul>
-    </div>
-    <Component {...pageProps}/>
-    <div className={styles.footer}>
-      <div className={styles.footFBox}>
-        example text
+      <div className={styles.topBar}>
+        <h1 className={styles.title}>
+          <style>
+          @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+          </style>
+            BEE CREATIVE
+        </h1>
+        <ul className={styles.toplist}>
+          <li className={styles.list}>Nav1</li>
+          <li className={styles.list}>Nav2</li>
+          <li className={styles.list}>Nav3</li>
+          <li className={styles.list}>Nav4</li>
+        </ul>
       </div>
-    </div>
+      <Component {...pageProps}/>
+      <div className={styles.footContainer}>
+        <div className={styles.footer}>
+          <div className={styles.footFBox}>
+            example text
+          </div>
+        </div>
+      </div>
     </div>
  )
 }
