be023efebcd4f5511fe1564839c52066ab7b1902
Toby Lynas <tobylynas@tobys-air.lan>
2020-12-17 15:17:21 +0000

    Squashed bug

--- pages/index.js
+++ pages/index.js
@@ -9,20 +9,6 @@ export default function newPage(){
         </Head>
 
         <main>
-            <div className={styles.topBar}>
-              <h1 className={styles.title}>
-                <style>
-                  @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
-                </style>
-                  BEE CREATIVE
-              </h1>
-              <ul className={styles.toplist}>
-                <li className={styles.list}>Nav1</li>
-                <li className={styles.list}>Nav2</li>
-                <li className={styles.list}>Nav3</li>
-                <li className={styles.list}>Nav4</li>
-              </ul>
-            </div>
         </main>
       </div>
     )
