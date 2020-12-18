e62a4881eaa0f4f9c00e990b85ecd16dc2c87b8b
Toby Lynas <tobylynas@tobys-air.lan>
2020-12-18 19:17:52 +0000

    add styling to image

--- components/gallery.js
+++ components/gallery.js
@@ -3,14 +3,17 @@ import styles from './gallery.module.css'
 function GalleryItem () {
   return (
     <div >
-      <Image
-      src="/gallery1.jpg"
-      className={styles.gallery}
-      width={450}
-      height={350}
-      />
+      <img src='gallery1.jpg' className={styles.imageOne}></img>
     </div>
   )
 }
 
 export default GalleryItem
+
+/*      <Image
+src="/gallery1.jpg"
+width={450}
+height={350}
+layout={`intrinsic`}
+className={imageOne}
+/> */
