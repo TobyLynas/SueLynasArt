0cdc8299796a2205ac2b41884b639797e53280cf
Toby Lynas <tobylynas@tobys-air.lan>
2020-12-19 18:23:00 +0000

    add Padding to text

--- components/gallery.js
+++ components/gallery.js
@@ -4,10 +4,12 @@ function GalleryItem () {
   return (
     <div className={styles.container}>
         <img src='gallery1.jpg' className={styles.imageOne}></img>
-        <div className={styles.heading}>SubHeading
-         <body className={styles.body}>
-         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-         </body>
+        <div className={styles.textBox}>
+          <div className={styles.heading}>SubHeading
+          <body className={styles.body}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </body>
+          </div>
         </div>
       </div>
   )
