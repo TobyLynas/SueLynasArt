04345681a7c64443b9f61f76f89d4fcaf054e220
Toby Lynas <tobylynas@tobys-air.lan>
2020-12-19 18:42:36 +0000

    squished bug changing top nav text color

--- components/gallery.js
+++ components/gallery.js
@@ -3,12 +3,11 @@ import styles from './gallery.module.css'
 function GalleryItem () {
   return (
     <div className={styles.container}>
-        <img src='gallery1.jpg' className={styles.imageOne}></img>
-        <div className={styles.textBox}>
-          <div className={styles.heading}>SubHeading
-          <body className={styles.body}>
+        <img src='gallery1.jpg' className={styles.galImage}></img>
+        <div className={styles.galTextBox}>
+          <div className={styles.galHeading}>SubHeading</div>
+          <div className={styles.galText}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </body>
           </div>
         </div>
       </div>
