c238a1cd50a02fc5fe1e3241a2e740a33b1b23b8
Toby Lynas <tobylynas@tobys-air.lan>
2020-12-19 18:16:31 +0000

    format element text

--- components/gallery.js
+++ components/gallery.js
@@ -4,7 +4,11 @@ function GalleryItem () {
   return (
     <div className={styles.container}>
         <img src='gallery1.jpg' className={styles.imageOne}></img>
-        <div className={styles.imageText}>Text </div>
+        <div className={styles.heading}>SubHeading
+         <body className={styles.body}>
+         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+         </body>
+        </div>
       </div>
   )
 }
