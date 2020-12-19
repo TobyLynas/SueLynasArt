ea4d8d953e6a38f6d5bbef0bfe3002bcbecc2548
Toby Lynas <tobylynas@tobys-air.lan>
2020-12-19 06:28:24 +0000

    Add text box to imaage.

--- components/gallery.js
+++ components/gallery.js
@@ -2,18 +2,11 @@ import Image from 'next/image'
 import styles from './gallery.module.css'
 function GalleryItem () {
   return (
-    <div >
-      <img src='gallery1.jpg' className={styles.imageOne}></img>
-    </div>
+    <div className={styles.container}>
+        <img src='gallery1.jpg' className={styles.imageOne}></img>
+        <div className={styles.imageText}>Text </div>
+      </div>
   )
 }
 
 export default GalleryItem
-
-/*      <Image
-src="/gallery1.jpg"
-width={450}
-height={350}
-layout={`intrinsic`}
-className={imageOne}
-/> */
