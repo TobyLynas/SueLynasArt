5385cbe2e7f3955f4b74f6528ba254ad08e02171
Toby Lynas <tobylynas@tobys-air.lan>
2020-12-19 19:29:07 +0000

    add padding and alter text

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,11 @@ export default function index(){
         </Head>
         
         <main>
-          <GalleryItem title="PLACEHOLDER" text="lorem ipsum nanananaiuiums" image="gallery1.jpg"/>
+          <div className={styles.gallery}>
+            <GalleryItem title="Projects" text="Check out my latest big projects and large works!" image="gallery1.jpg"/>
+            <GalleryItem title="Crafting" text="Crafting of all kinds! Have a look at my adorable festive decorations or browse pre-made kits!" image="crafting.webp"/>
+            <GalleryItem title="Painting" text="The center for all my painting projects and sessions! Click here to give it a Van Gough!" image="painting.jpg"/>
+          </div>
         </main>
       </div>
     )
