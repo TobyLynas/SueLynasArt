172921fb19a53a0ca5a556a2853dd8f03bccad7a
Toby Lynas <tobylynas@tobys-air.lan>
2020-12-20 09:34:35 +0000

    create pass/fail states for components

--- components/navselector.js
+++ components/navselector.js
@@ -2,7 +2,12 @@ import {useRouter} from 'next/router'
 
 function highlight (props) {
     const router = useRouter()
-    return(router.pathname)
+    if (router.pathname === props.title) {
+        return(<div>Success</div>)
+    }
+    else {
+        return(<div>Failure</div>)
+    }
 }
 
 export default highlight
