b19441aa52d23eec747e5e607ccb98ea69a8bb25
Toby Lynas <tobylynas@tobys-air.lan>
2020-12-20 23:00:03 +0000

    add links to top nav

--- components/navselector.js
+++ components/navselector.js
@@ -1,13 +1,13 @@
 import {useRouter} from 'next/router'
 import styles from '../components/navSelector.module.css'
-
+import Link from 'next/link'
 function highlight (props) {
     let router = useRouter()
     if(router.pathname === props.title){
-         return(<div><b className={styles.toplist}>{props.name}</b></div>)
+         return(<div><Link href={props.title}><b className={styles.toplist}>{props.name}</b></Link></div>)
         }
      else {
-        return(<div className={styles.toplist}>{props.name}</div>)
+        return(<div className={styles.toplist}><Link  href={props.title}>{props.name}</Link></div>)
     }
 }
 export default highlight
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/index.module.css'
-<<<<<<< HEAD
-import Gallery from '../components/gallery.js'
-=======
 import GalleryItem from '../components/gallery.js'
 
->>>>>>> master
 export default function index(){
   return(
       <div>
