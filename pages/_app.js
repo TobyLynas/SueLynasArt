6102b034057b010aa5fbb57bef20724d35b8f08c
Toby Lynas <tobylynas@tobys-air.lan>
2020-12-20 19:13:56 +0000

    seperate footer to component

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import styles from '../styles/index.module.css'
 import Head from 'next/head'
 import Highlight from '../components/navselector.js'
+import Footer from '../components/footer.js'
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -24,15 +25,7 @@ function MyApp({ Component, pageProps }) {
       <div>
         <img src="/honeycomb.svg" className={styles.honeycomb} />
       </div>
-      <div className={styles.footContainer}>
-        <div className={styles.footer}>
-          <div className={styles.footFBox}>
-            <img src="/facebook.svg" alt="FaceBook Logo" className={styles.logo} />
-            <img src="/instagram.svg" alt="Instagram Logo" className={styles.logo} />
-            <img src="/etsy.svg" alt="Etsy Logo" className={styles.logo} />
-          </div>
-        </div>
-      </div>
+    <Footer  />
     </div>
  )
 }
