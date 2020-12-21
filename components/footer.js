0c6a57d22d9ea121b31077f6f4c6a5ca0b9e116c
Toby Lynas <tobylynas@tobys-air.lan>
2020-12-21 09:49:32 +0000

    add enlarge effect on hover

--- components/footer.js
+++ components/footer.js
@@ -5,9 +5,15 @@ function Footer(props) {
         <div className={styles.footContainer}>
             <div className={styles.footer}>
                 <div className={styles.footFBox}>
-                    <Link href="https://www.facebook.com/beecreativeandbuzz"><img src="/facebook.svg" alt="FaceBook Logo" className={styles.logo} /></Link>
-                    <Link href="https://www.instagram.com/beecreative_and_buzz/"><img src="/instagram.svg" alt="Instagram Logo" className={styles.logo} /></Link>
-                    <Link href="https://www.etsy.com/uk/shop/beecreativeshop/"><img src="/etsy.svg" alt="Etsy Logo" className={styles.logo} /></Link>
+                    <div className={styles.enlarge}>
+                        <Link href="https://www.facebook.com/beecreativeandbuzz"><img src="/facebook.svg" alt="FaceBook Logo" className={styles.logo} /></Link>
+                    </div>
+                    <div className={styles.enlarge}>
+                        <Link href="https://www.instagram.com/beecreative_and_buzz/"><img src="/instagram.svg" alt="Instagram Logo" className={styles.logo} /></Link>
+                    </div>
+                    <div className={styles.enlarge}>
+                        <Link href="https://www.etsy.com/uk/shop/beecreativeshop/"><img src="/etsy.svg" alt="Etsy Logo" className={styles.logo} /></Link>
+                    </div>
                 </div>
             </div>
         </div>
