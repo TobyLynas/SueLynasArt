7cdc449ef1e1a2f12e814d0c05cd25f02b921fae
Toby Lynas <tobylynas@tobys-air.lan>
2020-12-27 18:32:36 +0000

    Created PaintingGallery component and .css file

---
+++ components/paintingGallery.js
@@ -0,0 +1,9 @@
+import styles from '../components/paintingGallery.module.css'
+
+function paintingGallery(props) {
+    return(
+        <div className={styles.main}></div>
+    )
+}
+
+export default paintingGallery
--- pages/painting.js
+++ pages/painting.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/painting.module.css'
+import PaintingGallery from '../components/paintingGallery.js'
 
 export default function newPage(){
   return(
@@ -9,6 +10,7 @@ export default function newPage(){
         </Head>
         <main className={styles.page}>
           <div className={styles.pageDisc}>This is the painting page!<br/><br/>A longer discriptipn goes here! Filler - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
+          <PaintingGallery></PaintingGallery>
         </main>
     </div>
   )
