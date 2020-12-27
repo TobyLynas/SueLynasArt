a8167f7a4a0aa1e0f090c5e3277d2ff81c6cc10e
Toby Lynas <tobylynas@tobys-air.lan>
2020-12-27 12:17:05 +0000

    add styling to text box

--- components/craftingBox.js
+++ components/craftingBox.js
@@ -4,6 +4,11 @@ function boxImg(props) {
     return(
         <div className={styles.box}>
             <img className={styles.img} src={props.img} />
+            <div className={styles.childBox}>{props.title}<br /><br />
+            <div className={styles.text}>
+                {props.text}
+            </div>
+            </div>
         </div>
     )
 }
