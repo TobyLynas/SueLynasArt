3cde2b5a5b5b40b5b6b151bbe72c7e76fb675ae2
Toby Lynas <tobylynas@tobys-air.lan>
2020-12-27 18:18:51 +0000

    Add styling pages and intro descriptor box

--- pages/painting.js
+++ pages/painting.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import styles from '../styles/index.module.css'
+import styles from '../styles/painting.module.css'
 
 export default function newPage(){
   return(
@@ -7,6 +7,9 @@ export default function newPage(){
         <Head>
             <title>Painting!</title>
         </Head>
+        <main className={styles.page}>
+          <div className={styles.pageDisc}>This is the painting page!<br/><br/>A longer discriptipn goes here! Filler - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
+        </main>
     </div>
   )
 }
