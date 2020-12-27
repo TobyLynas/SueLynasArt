c6420f40d9f8f14132d7572c00bcc721e08ab6ab
Toby Lynas <tobylynas@tobys-air.lan>
2020-12-27 12:59:13 +0000

    add left/right logic to craftingBox component

--- components/craftingBox.js
+++ components/craftingBox.js
@@ -1,16 +1,29 @@
 import styles from './craftingBox.module.css'
-
 function boxImg(props) {
+    if (props.side==='right'){
+        return(
+            <div className={styles.box}>
+                <img className={styles.img} src={props.img} />
+                <div className={styles.childBox}>
+                    {props.title}<br /><br />
+                    <div className={styles.text}>{props.text}</div>
+                </div>
+            </div>
+        )
+    }
+
+else {
     return(
         <div className={styles.box}>
-            <img className={styles.img} src={props.img} />
-            <div className={styles.childBox}>{props.title}<br /><br />
-            <div className={styles.text}>
-                {props.text}
-            </div>
+            <img className={styles.imgLeft} src={props.img} />
+            <div className={styles.childBoxLeft}>
+                {props.title}<br /><br />
+                <div className={styles.text}>{props.text}</div>
             </div>
         </div>
     )
 }
+}
+    
 
 export default boxImg
