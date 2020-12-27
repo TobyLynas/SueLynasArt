3495039502e81a6f9393d70c6b93f9d8573eec4c
Toby Lynas <tobylynas@tobys-air.lan>
2020-12-27 17:32:37 +0000

    Altered logic for aligning to a chosen side

--- components/boxImg.js
+++ components/boxImg.js
@@ -1,29 +1,16 @@
 import styles from './boxImg.module.css'
 function boxImg(props) {
-    if (props.side==='right'){
-        return(
-            <div className={styles.box}>
-                <img className={styles.img} src={props.img} />
-                <div className={styles.childBox}>
-                    {props.title}<br /><br />
-                    <div className={styles.text}>{props.text}</div>
-                </div>
-            </div>
-        )
-    }
-
-else {
+    const isRight = props.align=='right'
     return(
         <div className={styles.box}>
-            <img className={styles.imgLeft} src={props.img} />
-            <div className={styles.childBoxLeft}>
+            <img className={isRight?styles.img:styles.imgLeft} src={props.img} />
+            <div className={isRight?styles.childBox:styles.childBoxLeft}>
                 {props.title}<br /><br />
                 <div className={styles.text}>{props.text}</div>
             </div>
         </div>
     )
-}
-}
+    }
     
 
 export default boxImg
