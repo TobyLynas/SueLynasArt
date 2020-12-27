52878eca6108e633716a9c44cfdf143fa9560004
Toby Lynas <tobylynas@tobys-air.lan>
2020-12-27 12:02:44 +0000

    add Image to component and adjust sizing

--- components/craftingBox.js
+++ components/craftingBox.js
@@ -3,6 +3,7 @@ import styles from './craftingBox.module.css'
 function boxImg(props) {
     return(
         <div className={styles.box}>
+            <img className={styles.img} src={props.img} />
         </div>
     )
 }
--- pages/crafting.js
+++ pages/crafting.js
@@ -9,7 +9,7 @@ export default function newPage(){
             <title>Crafting!</title>
         </Head>
         <main className={styles.main}>
-          <BoxImg></BoxImg>
+          <BoxImg img="CraftingImg-1.png" title="Lorem Ipsum" text="Ut ipsum tortor, pulvinar eget enim nec, ultrices aliquam lorem. In vitae maximus lectus. Donec tristique, felis vitae dictum fringilla, arcu velit maximus dolor, at pretium arcu massa molestie nisl. Ut gravida, nulla nec lobortis faucibus, nunc odio imperdiet urna, ac malesuada nibh dolor vitae nisl"></BoxImg>
         </main>
     </div>
   )
