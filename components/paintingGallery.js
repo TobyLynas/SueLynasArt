8f75045cd2f8e1151f604a1e79004877f9965308
Toby Lynas <tobylynas@tobys-air.lan>
2020-12-27 18:46:42 +0000

    Setup grid container and position first row of boxes

--- components/paintingGallery.js
+++ components/paintingGallery.js
@@ -2,7 +2,12 @@ import styles from '../components/paintingGallery.module.css'
 
 function paintingGallery(props) {
     return(
-        <div className={styles.main}></div>
+        <div className={styles.main}>
+            <div className={styles.item1}></div>
+            <div className={styles.item2}></div>
+            <div className={styles.item3}></div>
+
+        </div>
     )
 }
 
