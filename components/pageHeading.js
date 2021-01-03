6a7e97ca80bbb0f49e4e6b2e445cf9e05b33355b
Toby Lynas <tobylynas@tobys-air.lan>
2021-01-03 12:50:01 +0000

    Add styling to fit figma design

--- components/pageHeading.js
+++ components/pageHeading.js
@@ -2,7 +2,13 @@ import styles from '../components/pageHeading.module.css'
 
 function pageHeading(props) {
     return(
-        <div> 
+        <div className={styles.main}> 
+            <div className={styles.box}>
+                <div className={styles.title}>
+                    {props.title}
+                <div className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at dui neque.</div>
+                </div>
+            </div>
         </div>
     )
 }
