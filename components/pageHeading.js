2934b16f3aa5276d60cf0ee476ae01890baed83c
Toby Lynas <tobylynas@tobys-air.lan>
2021-01-03 12:25:08 +0000

    Setup page and pass props

---
+++ components/pageHeading.js
@@ -0,0 +1,11 @@
+import styles from '../components/pageHeading.module.css'
+
+function pageHeading(props) {
+    return(
+        <div> 
+        </div>
+    )
+}
+
+
+export default pageHeading
--- pages/painting.js
+++ pages/painting.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/painting.module.css'
-import ImgGrid from '../components/imgGrid.js'
+import PageHeading from '../components/pageHeading.js'
 
 export default function newPage(){
   return(
@@ -9,8 +9,7 @@ export default function newPage(){
             <title>Painting!</title>
         </Head>
         <main className={styles.page}>
-          <div className={styles.pageDisc}>This is the painting page!<br/><br/>A longer discriptipn goes here! Filler - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</div>
-          <ImgGrid imgSources={['painting1.jpg','painting2.jpg','painting3.jpg','painting4.jpg','painting5.jpg','painting6.jpg','painting7.jpg','painting8.jpg','painting9.jpg']}></ImgGrid>
+          <PageHeading title="Painting!" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at dui neque."></PageHeading>
         </main>
     </div>
   )
