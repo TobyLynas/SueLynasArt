bea08d866c5005997334f1615b7b101862999bd2
Toby Lynas <tobylynas@tobys-air.lan>
2021-01-03 13:50:40 +0000

    Style the contact me component

--- components/contactMe.js
+++ components/contactMe.js
@@ -2,7 +2,19 @@ import styles from '../components/contactMe.module.css'
 
 function contactMe(props){
     return(
-        <div>yes</div>  
+        <div className={styles.main}>
+            <div className={styles.topBox}>
+                Sue Lynas - I’m a designer and maker, I love to draw, paint and make things. I’m always happy to chat if you have a special commission in mind.
+            </div>
+            <div className={styles.gap}/>
+            <div className={styles.bottomBox}>
+                CONTACT ME <br/>
+                <div className={styles.subText}>
+                    07845053275<br/>
+                    beecreativeoffice@gmail.com
+                </div>
+            </div>
+        </div>  
     )
 }
 
