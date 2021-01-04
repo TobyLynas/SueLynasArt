accb6973d6133016145e7cbb87a31fe8956beb64
Toby Lynas <tobylynas@tobys-air.lan>
2021-01-04 17:07:42 +0000

    Fixed postion on page

--- components/scrollGallery.js
+++ components/scrollGallery.js
@@ -2,17 +2,20 @@ import styles from '../components/scrollGallery.module.css'
 
 function scrollGallery(props){
     return(
-        <div>
-            <img className={styles.img}src='craftingImg.png'/>
-            <div className={styles.childBox}>Lorem Ipsum</div>
-            <div className={styles.buttons}>
-                <div className={styles.LButton}>
-                    <img src='LTri.svg'/>
+        <div className={styles.page}>
+            <div className={styles.container}>
+                <img className={styles.img}src='craftingImg.png'/>
+                <div className={styles.textBox}>Lorem Ipsum
+                    <div className={styles.buttons}>
+                        <div className={styles.LButton}>
+                            <img src='LTri.svg'/>
+                        </div>
+                        <div className={styles.RButton}>
+                            <img src='RTri.svg'/>
+                        </div>
+                    </div>
+                    </div>
                 </div>
-                <div className={styles.RButton}>
-                    <img src='RTri.svg'/>
-                </div>
-            </div>
         </div>
     )
 }
