3a0855fc94b725c4183d5ece998fc63fb3af972a
Toby Lynas <tobylynas@tobys-air.lan>
2021-01-04 11:36:44 +0000

    add Arrows to page

--- components/scrollGallery.js
+++ components/scrollGallery.js
@@ -5,6 +5,14 @@ function scrollGallery(props){
         <div>
             <img className={styles.img}src='craftingImg.png'/>
             <div className={styles.childBox}>Lorem Ipsum</div>
+            <div className={styles.buttons}>
+                <div className={styles.LButton}>
+                    <img src='LTri.svg'/>
+                </div>
+                <div className={styles.RButton}>
+                    <img src='RTri.svg'/>
+                </div>
+            </div>
         </div>
     )
 }
