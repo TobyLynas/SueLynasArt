a7c020b9ff9248f0f13f6ed127633651da15259b
Toby Lynas <tobylynas@tobys-air.lan>
2021-01-18 14:17:10 +0000

    add logic to move index up and down according to button click

--- components/scrollGallery.js
+++ components/scrollGallery.js
@@ -3,22 +3,25 @@ import { useState } from 'react'
 import styles from '../components/scrollGallery.module.css'
 
 function scrollGallery(props){
-    function handleClick() {
-        console.log('Win')
+    let picIndex = 0
+    function clickDown() {
+        picIndex --;
+        console.log(picIndex);
     }
-    
-    /*const [photoIndex, setPhotoIndex] = useState(0)       () => handleClick()
-    console.log(photoIndex)       setPhotoIndex(photoIndex+1)*/
+    function clickUp() {
+        picIndex ++;
+        console.log(picIndex);
+    }
         return(
         <div className={styles.page}>
             <div className={styles.container}>
                 <img className={styles.img}src='craftingImg.png'/>
                 <div className={styles.textBox}>Lorem Ipsum
                     <div className={styles.buttons}>
-                        <button type ='button' onClick={()=>{handleClick()}} className={styles.LButton}>
+                        <button type ='button' onClick={()=>{clickDown()}} className={styles.LButton}>
                             <img src='LTri.svg'/>
                         </button>
-                        <button type='button'  className={styles.RButton}>
+                        <button type='button' onClick={()=>{clickUp()}} className={styles.RButton}>
                             <img src='RTri.svg'/>
                         </button>
                     </div>
