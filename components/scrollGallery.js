a0f14d56060c4adb561e2ffde8357500038e827e
Toby Lynas <tobylynas@tobys-air.lan>
2021-01-21 09:39:51 +0000

    Added array picDir and use in place of picIndex+png in picture src

--- components/scrollGallery.js
+++ components/scrollGallery.js
@@ -1,20 +1,17 @@
 import Router from 'next/dist/next-server/lib/router/router'
 import styles from '../components/scrollGallery.module.css'
-import * as React from 'react'
-import componentDidMount from 'react';
-import setState from 'react'
-import forceUpdate from 'react'
 import { useState } from 'react';
 
 //picDir = ['1.png', '2.png']
 
 function scrollGallery(props){
-    const [picIndex, setPicIndex] = useState(1);
+    const picDir = ['1.png', '2.png', '3.png']
+    const [picIndex, setPicIndex] = useState(0);
     
         return(
         <div className={styles.page}>
             <div className={styles.container}>
-                <img className={styles.img} src= {picIndex + '.png'} />
+                <img className={styles.img} src= {picDir[picIndex]} />
                 <div className={styles.textBox}>Lorem Ipsum
                     <div className={styles.buttons}>
                         <button type ='button' onClick={() => setPicIndex(picIndex - 1)} className={styles.LButton}>
