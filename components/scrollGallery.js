aee2d6fc2283635b9b560622d982cb4c66cf9b7a
Toby Lynas <tobylynas@tobys-air.lan>
2021-01-21 00:34:45 +0000

    Changed logic to allow element to update when index value is changed

--- components/scrollGallery.js
+++ components/scrollGallery.js
@@ -1,27 +1,26 @@
 import Router from 'next/dist/next-server/lib/router/router'
-import { useState } from 'react'
 import styles from '../components/scrollGallery.module.css'
+import * as React from 'react'
+import componentDidMount from 'react';
+import setState from 'react'
+import forceUpdate from 'react'
+import { useState } from 'react';
+
+//picDir = ['1.png', '2.png']
 
 function scrollGallery(props){
-    let picIndex = 0
-    function clickDown() {
-        picIndex --;
-        console.log(picIndex);
-    }
-    function clickUp() {
-        picIndex ++;
-        console.log(picIndex);
-    }
+    const [picIndex, setPicIndex] = useState(1);
+    
         return(
         <div className={styles.page}>
             <div className={styles.container}>
-                <img className={styles.img}src='craftingImg.png'/>
+                <img className={styles.img} src= {picIndex + '.png'} />
                 <div className={styles.textBox}>Lorem Ipsum
                     <div className={styles.buttons}>
-                        <button type ='button' onClick={()=>{clickDown()}} className={styles.LButton}>
+                        <button type ='button' onClick={() => setPicIndex(picIndex - 1)} className={styles.LButton}>
                             <img src='LTri.svg'/>
                         </button>
-                        <button type='button' onClick={()=>{clickUp()}} className={styles.RButton}>
+                        <button type='button' onClick={() => setPicIndex(picIndex + 1)} className={styles.RButton}>
                             <img src='RTri.svg'/>
                         </button>
                     </div>
