092b2859bb81b57f767e11d304a6bfaad1b66851
Toby Lynas <tobylynas@tobys-air.lan>
2021-01-21 13:41:01 +0000

    Used map to create buttons and made buttons changed displayed image

--- components/scrollGallery.js
+++ components/scrollGallery.js
@@ -1,15 +1,20 @@
 import styles from '../components/scrollGallery.module.css'
 import { useState } from 'react';
 
+
+const ScrollGalleryButton = (props) => {
+    return (
+        <button onClick={props.handleClick} className={styles.barBut}> <img className={styles.childImg} src={props.image}/> </button>
+      )
+}
 function scrollGallery(props){
-    const picDir = ['1.png', '2.png', '3.png']
+    const picDir = ['1.png', '2.png', '3.png', ]
     const [picIndex, setPicIndex] = useState(0);
-    if (picIndex === 3) { //value should be last index number of picDir
+    if (picIndex === picDir.length) { //value should be last index number of picDir
         setPicIndex(0)
     } if (picIndex===-1){ 
-        setPicIndex(2) //value should be last index number of picDirs - 1 to account for arr indexing
+        setPicIndex(picDir.length-1) //value should be last index number of picDirs - 1 to account for arr indexing
     } 
-    
     return(
     <div className={styles.page}>
         <div className={styles.container}>
@@ -25,16 +30,7 @@ function scrollGallery(props){
                 </div>
             </div>
             <div className={styles.selBar}>
-                <button className={styles.But1} ><img className={styles.childImg} src='craftingImg.png' /></button>
-                <button className={styles.But1} ></button>
-                <button className={styles.But1} ></button>
-                <button className={styles.But1} ></button>
-                <button className={styles.But1} ></button>
-                <button className={styles.But1} ></button>
-                <button className={styles.But1} ></button>
-                <button className={styles.But1} ></button>
-                <button className={styles.But1} ></button>
-
+                {picDir.map((img, i) => <ScrollGalleryButton handleClick={() => {setPicIndex(i)}} image={img} />)}
             </div>
         </div>
 
