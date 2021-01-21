6e67d40b4f7fdc0a4be0292b7fba26ab3d68f90b
Toby Lynas <tobylynas@tobys-air.lan>
2021-01-21 10:10:35 +0000

    tidied code and added comments

--- components/scrollGallery.js
+++ components/scrollGallery.js
@@ -1,34 +1,31 @@
-import Router from 'next/dist/next-server/lib/router/router'
 import styles from '../components/scrollGallery.module.css'
 import { useState } from 'react';
-
-//picDir = ['1.png', '2.png']
 
 function scrollGallery(props){
     const picDir = ['1.png', '2.png', '3.png']
     const [picIndex, setPicIndex] = useState(0);
-    if (picIndex === 3) {
+    if (picIndex === 3) { //value should be last index number of picDir
         setPicIndex(0)
-    } if (picIndex===-1){
-        setPicIndex(3)
+    } if (picIndex===-1){ 
+        setPicIndex(3) //value should be last index number of picDirs
     } 
     
-        return(
-        <div className={styles.page}>
-            <div className={styles.container}>
-                <img className={styles.img} src= {picDir[picIndex]} />
-                <div className={styles.textBox}>Lorem Ipsum
-                    <div className={styles.buttons}>
-                        <button type ='button' onClick={() => setPicIndex(picIndex - 1)} className={styles.LButton}>
-                            <img src='LTri.svg'/>
-                        </button>
-                        <button type='button' onClick={() => setPicIndex(picIndex + 1)} className={styles.RButton}>
-                            <img src='RTri.svg'/>
-                        </button>
-                    </div>
-                    </div>
+    return(
+    <div className={styles.page}>
+        <div className={styles.container}>
+            <img className={styles.img} src= {picDir[picIndex]} />
+            <div className={styles.textBox}>Lorem Ipsum
+                <div className={styles.buttons}>
+                    <button type ='button' onClick={() => setPicIndex(picIndex - 1)} className={styles.LButton}>
+                        <img src='LTri.svg'/>
+                    </button>
+                    <button type='button' onClick={() => setPicIndex(picIndex + 1)} className={styles.RButton}>
+                        <img src='RTri.svg'/>
+                    </button>
                 </div>
+            </div>
         </div>
+    </div>
     )
 }
 
