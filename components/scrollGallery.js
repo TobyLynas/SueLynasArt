9c0dae8bb1910667d514cc61f28c5e664a5f41c1
Toby Lynas <tobylynas@Tobys-Air.lan>
2021-01-22 12:24:21 +0000

    Styled component

--- components/scrollGallery.js
+++ components/scrollGallery.js
@@ -10,10 +10,10 @@ const ScrollGalleryButton = (props) => {
 function scrollGallery(props){
     const picDir = ['1.png', '2.png', '3.png', 'craftingImg.png', 'crafting.webp']
     const [picIndex, setPicIndex] = useState(0);
-    if (picIndex === picDir.length) { //value should be last index number of picDir
+    if (picIndex === picDir.length) {
         setPicIndex(0)
     } if (picIndex===-1){ 
-        setPicIndex(picDir.length-1) //value should be last index number of picDirs - 1 to account for arr indexing
+        setPicIndex(picDir.length-1)
     } 
     return(
     <div className={styles.page}>
@@ -22,10 +22,10 @@ function scrollGallery(props){
             <div className={styles.textBox}>Lorem Ipsum
                 <div className={styles.buttons}>
                     <button type ='button' onClick={() => setPicIndex(picIndex - 1)} className={styles.LButton}>
-                        <img src='LTri.svg'/>
+                        <img className={styles.butImg} src='LTri.svg'/>
                     </button>
                     <button type='button' onClick={() => setPicIndex(picIndex + 1)} className={styles.RButton}>
-                        <img src='RTri.svg'/>
+                        <img className={styles.butImg} src='RTri.svg'/>
                     </button>
                 </div>
             </div>
