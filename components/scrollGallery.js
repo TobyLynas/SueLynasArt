d9e63698425e46e0bf9aa781e9e55ef74e6658fa
Toby Lynas <tobylynas@tobys-air.lan>
2021-01-22 18:15:26 +0000

    add final styling before peer review

--- components/scrollGallery.js
+++ components/scrollGallery.js
@@ -19,7 +19,8 @@ function scrollGallery(props){
     <div className={styles.page}>
         <div className={styles.container}>
             <img className={styles.img} src= {picDir[picIndex]} />
-            <div className={styles.textBox}>Lorem Ipsum
+            <div className={styles.textBox}>Lorem Ipsum <br/>
+            <div className={styles.subtext}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet lacus vitae neque sodales pretium at ut lectus. Maecenas fringilla condimentum felis eget semper. Cras congue in massa at lacinia. Phasellus efficitur, nisi nec commodo gravida, odio libero maximus felis, ut convallis risus augue et ante. Mauris sed lobortis lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vitae bibendum odio, a rutrum lorem.</div>
                 <div className={styles.buttons}>
                     <button type ='button' onClick={() => setPicIndex(picIndex - 1)} className={styles.LButton}>
                         <img className={styles.butImg} src='LTri.svg'/>
