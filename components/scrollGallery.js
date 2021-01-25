af8ef82d227cca9e440e4e72426386d5544d178c
Toby Lynas <tobylynas@tobys-air.lan>
2021-01-25 10:37:31 +0000

    Altered variable naming to improve readability

--- components/scrollGallery.js
+++ components/scrollGallery.js
@@ -8,31 +8,31 @@ const ScrollGalleryButton = (props) => {
       )
 }
 function scrollGallery(props){
-    const picDir = ['1.png', '2.png', '3.png', 'craftingImg.png', 'crafting.webp']
+    const picArr = ['1.png', '2.png', '3.png', 'craftingImg.png', 'crafting.webp']
     const textArr = ['Lorem ipsum', 'Lorem Ipsum 2', 'Lorem ipsum 3', 'Lorem ispum 4', 'Lorem ipsum 5']
     const titleArr = ['Title 1', ' title 2', 'title3', 'title 4','title 5']
-    const [picIndex, setPicIndex] = useState(0);
-    if (picIndex === picDir.length) {
+    const [index, setPicIndex] = useState(0);
+    if (index === picArr.length) {
         setPicIndex(0)
-    } if (picIndex===-1){ 
-        setPicIndex(picDir.length-1)
+    } if (index===-1){ 
+        setPicIndex(picArr.length-1)
     } 
     return(
     <div className={styles.page}>
         <div className={styles.container}>
-            <img className={styles.img} src= {picDir[picIndex]} />
-            <div className={styles.textBox}>{titleArr[picIndex]} <br/>
-            <div className={styles.subtext}>{textArr[picIndex]}</div>
+            <img className={styles.img} src= {picArr[index]} />
+            <div className={styles.textBox}>{titleArr[index]} <br/>
+            <div className={styles.subText}>{textArr[index]}</div>
             </div>
-            <div className={styles.selBar}>
-                {picDir.map((img, i) => <ScrollGalleryButton class={img===picDir[picIndex]?styles.found:styles.barBut}  handleClick={() => {setPicIndex(i)}} image={img} />)}
+            <div className={styles.selectorBar}>
+                {picArr.map((img, i) => <ScrollGalleryButton class={img===picArr[index]?styles.found:styles.barButton}  handleClick={() => {setPicIndex(i)}} image={img} />)}
             </div>
             <div className={styles.buttons}>
-                <button type ='button' onClick={() => setPicIndex(picIndex - 1)} className={styles.LButton}>
-                    <img className={styles.butImg} src='LTri.svg'/>
+                <button type ='button' onClick={() => setPicIndex(index - 1)} className={styles.LButton}>
+                    <img className={styles.buttonImg} src='LTri.svg'/>
                 </button>
-                <button type='button' onClick={() => setPicIndex(picIndex + 1)} className={styles.RButton}>
-                    <img className={styles.butImg} src='RTri.svg'/>
+                <button type='button' onClick={() => setPicIndex(index + 1)} className={styles.RButton}>
+                    <img className={styles.buttonImg} src='RTri.svg'/>
                 </button>
             </div>
         </div>
